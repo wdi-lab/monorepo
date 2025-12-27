73410c4223ed1f9c5845f839db30ed0f239843b6
Hoang Nguyen <contact@hoangnd.me>
2025-12-27 19:44:51 +1100

    update hosted zone

--- services/infra-aws-account/src/app.ts
+++ services/infra-aws-account/src/app.ts
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { AccountDnsStack } from './stacks/dns.ts';
 import { regions } from '@lib/sst-helpers';
+import { AccountDNSStack } from './stacks/dns.ts';
 
 const app = new cdk.App();
 
 const region = regions.getHomeRegion(); // process.env.CDK_DEFAULT_REGION
 const account = process.env.CDK_DEFAULT_ACCOUNT;
 
-new AccountDnsStack(app, 'AccountDns', {
+new AccountDNSStack(app, 'AccountDNS', {
   env: { account, region },
   description: 'AWS Account shared DNS stack',
 });
--- services/infra-aws-account/src/stacks/dns.ts
+++ services/infra-aws-account/src/stacks/dns.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { dns } from '@lib/sst-helpers';
 
-export class AccountDnsStack extends cdk.Stack {
+export class AccountDNSStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
